df54144a17e70c19f6dd064a24e3cc3d838ba162
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-08-31 18:14:17 +0300

    Create Student,Adress and Gender Entities

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Adress.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentAdminPortal.API.DataModels
+{
+    public class Adress
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAdress { get; set; }
+
+        public string PostalAdress { get; set; }
+
+        public Guid StudentId { get; set; }
+    }
+}
