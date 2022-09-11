a017dd449f93927fe6f841ae66af11d1bafd7655
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-09-11 16:33:46 +0300

    Create GendersController

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/GendersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class GendersController : Controller
+    {
+        [HttpGet]
+        [Route("[controller]")]
+        public IActionResult GetAllGenders()
+        {
+            return View();
+        }
+    }
+}
